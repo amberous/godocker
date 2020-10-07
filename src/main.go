99fac49a2ac42e52780382e05c610ef2ff687770
Blake Tiemann <blake.higgins36@gmail.com>
2020-10-07 15:03:18 -0500

    NewServeMux

--- src/main.go
+++ src/main.go
@@ -22,16 +22,19 @@ func (handler HttpHandler) ServeHTTP(res http.ResponseWriter, req *http.Request)
     res.Write([]byte("❤️"))
 }
 
-func (handler HttpHandler) ListenAndServe(port string) {
-	err := http.ListenAndServe(":8080", handler)
+func main() {
+	kernal := http.NewServeMux()
+	kernal.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		io.WriteString(res, "Hello World!")
+	})
+	kernal.HandleFunc("/go", func(res http.ResponseWriter, req *http.Request) {
+		io.WriteString(res, "Hello Go!")
+	})
+
+	err := http.ListenAndServe(":8080", kernal)
 	
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 		return
 	}
-}
-
-func main() {
-	kernal := HttpHandler{}
-	kernal.ListenAndServe(":8080")
 }
